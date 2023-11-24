f9acd870b4d7968e98ba6672869b8c3a7b68c519
pendulum <75574213+shkvik@users.noreply.github.com>
2023-11-24 16:41:47 +0600

    Update searchInsertPosition.hpp

--- с++/algorithms/easy/searchInsertPosition.hpp
+++ с++/algorithms/easy/searchInsertPosition.hpp
@@ -9,10 +9,8 @@ public:
     int searchInsert(const std::vector<int>& nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
             if (nums[mid] == target) {
                 return mid; 
             }
@@ -23,7 +21,6 @@ public:
                 right = mid - 1; 
             }
         }
-
         return left;
     }
 };
